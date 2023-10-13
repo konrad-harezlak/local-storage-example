690fb472c8a30f6156826919fc8b0806c9018930
konrad-harezlak <konrad.harezlakk@gmail.com>
2023-10-13 10:39:52 +0200

    Fix localStorage retrive and display

--- java.js
+++ java.js
@@ -1,15 +1,21 @@
-function MyFunction() {
+let myButton2=document.getElementById('myButton2');
+myButton2.addEventListener('click',function() {
     if (typeof (Storage) !== "undefined") {
-        // Store
-        
         // Retrieve
-        document.getElementById("localstorage").innerHTML = "Imie: " + localStorage.firstname
-        document.getElementById("result").innerHTML = "Nazwisko: " + localStorage.getItem("lastname");
+        let localStorage2 = document.getElementsByClassName("localstorage")[1];
+        let firstName=localStorage.firstname;
+        let lastName=localStorage.lastname;
+        localStorage2.innerHTML=`
+        <p>Name:${firstName}<p>
+        <p> Surname:${lastName}<p>
+        `   
+        console.log(firstName,lastName)
     }
     else {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
     }
-}
+});
+ // Store
 let myButton = document.getElementById('myButton');
 myButton.addEventListener('click',function(){
     let name = document.getElementsByName('name');
